2f56fb4e4e55d3b108347dfb2eef36ca7253c9bb
brandon-le-1 <brandole16@gmail.com>
2022-01-13 13:04:45 -0700

    edit/delete htmls added

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,5 @@ class Item < ApplicationRecord
     validates :name, presence: true
     validates :quantity, presence: true
     validates :price, presence: true
-    validates :description, length: { maximum: 150,
-    error_message: "150 characters is the limit for item description. Please try again." }
+    validates :description, length: { maximum: 150}
 end

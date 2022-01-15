e2213fd72789272a3833ab8d5a1f7f4fcf92d090
brandon-le-1 <brandole16@gmail.com>
2022-01-14 18:56:01 -0700

    fixed root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "welcome#index"
+  root "items#index"
  
   get '/items', to: 'items#index'
   get '/items/new', to: 'items#new'

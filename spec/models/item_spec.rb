958dbd2d396fec0eaa93790ea90b8c6c33c9d401
brandon-le-1 <brandole16@gmail.com>
2022-01-18 13:44:30 -0700

    rspec tests added

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'exists' do
+    item = Item.new(name: 'Banana', quantity: 2, description: 'Yellow fruit that must be peeled before eaten.', price: 2.15)
+
+    expect(item).to be_an_instance_of(Item)
+  end
+
+  it "has a name, quantity, description, and price" do 
+    item = Item.new(name: 'Banana', quantity: 2, description: 'Yellow fruit that must be peeled before eaten.', price: 2.15)
+    expected = 'Yellow fruit that must be peeled before eaten.'
+    
+    expect(item.name).to eq('Banana')
+    expect(item.quantity).to eq(2)
+    expect(item.description).to eq(expected)
+    expect(item.price). to eq(2.15)
+  end
 end
---
+++ spec/requests/methods_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe "Items", type: :feature do
+    it "can see all items currently in inventory and their attirbutes" do
+        item_1 = Item.create(name: 'Banana', quantity: 2, description: 'Yellow fruit that must be peeled before eaten.', price: 2.15)
+        item_2 = Item.create(name: 'Pineapple', quantity: 3, description: 'Sweet spiky fruit. Be careful!', price: 5.26)
+    
+        visit "/items"
+    
+        expect(page).to have_content(item_1.name)  
+        expect(page).to have_content(item_1.quantity)
+        expect(page).to have_content(item_1.description)
+        expect(page).to have_content(item_1.price)  
+        
+        expect(page).to have_content(item_2.name)  
+        expect(page).to have_content(item_2.quantity)
+        expect(page).to have_content(item_2.description)
+        expect(page).to have_content(item_2.price)     
+    end
+
+    it "add an item to the inventory" do
+        item = Item.create(name: 'Banana', quantity: 2, description: 'Yellow fruit that must be peeled before eaten.', price: 2.15)
+
+        expect(Item.where(name: 'Banana', quantity: 2, description: 'Yellow fruit that must be peeled before eaten.', price: 2.15 )).to exist
+    end
+
+    it "deletes inventory properly" do
+        item = Item.create(name: 'Banana', quantity: 2, description: 'Yellow fruit that must be peeled before eaten.', price: 2.15)
+
+        expect(Item.where(name: 'Banana', quantity: 2, description: 'Yellow fruit that must be peeled before eaten.', price: 2.15 )).to exist
+
+        item.destroy
+
+        expect(Item.where(name: 'Banana', quantity: 2, description: 'Yellow fruit that must be peeled before eaten.', price: 2.15 )).not_to exist
+
+    end
+
+    it "edits inventory properly" do
+        item = Item.create(name: 'Banana', quantity: 2, description: 'Yellow fruit that must be peeled before eaten.', price: 2.15)
+
+        expect(Item.where(name: 'Banana', quantity: 2, description: 'Yellow fruit that must be peeled before eaten.', price: 2.15 )).to exist
+
+        item.update(name: 'Banana', quantity: 5, description: 'Yellow fruit that must be peeled before eaten.', price: 2.15)
+
+        expect(Item.where(name: 'Banana', quantity: 2, description: 'Yellow fruit that must be peeled before eaten.', price: 2.15 )).not_to exist
+        expect(Item.where(name: 'Banana', quantity: 5, description: 'Yellow fruit that must be peeled before eaten.', price: 2.15 )).to exist
+    end
+end
